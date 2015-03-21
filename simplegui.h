4b136f36b8743fb0547a3bf989ea09476d0b9b22
Gonzalo Hernández <chalo@localhost.localdomain>
2015-03-21 21:42:39 +0000

    Post programming event using pointer functions.

--- simplegui.h
+++ simplegui.h
@@ -5,7 +5,10 @@
 #include <cstring>
 #include <iostream>
 #include <X11/Xutil.h>
-
+/**
+ * @brief The Widget class
+ * ==================================================================================
+ */
 class Widget {
 protected:
     int x,y;
@@ -16,10 +19,17 @@ protected:
     int         screen;
     Window      window;
     GC          gc;
+
+    //-------------------------------------------------------------------------------
+    static void defaultAction() {}
+
 public:
+    void (*action)();
+
     Widget(int x,int y,int width,int height,const char text[])
         : x(x), y(y), width(width), height(height), focused(false) {
         strcpy(this->text,text);
+        action = &defaultAction;
     }
     //-------------------------------------------------------------------------------
     void updateGraphVariables(Display* display,int screen,Window window,GC gc) {
@@ -53,9 +63,14 @@ public:
     }
 };
 
+/**
+ * @brief The Label class
+ * ==================================================================================
+ */
 class Label : public Widget {
 public:
-    Label(int x,int y,int width,int height,const char text[]) : Widget(x,y,width,height,text){
+    Label(int x,int y,int width,int height,const char text[])
+        : Widget(x,y,width,height,text){
     }
     //-------------------------------------------------------------------------------
     void draw() {
@@ -80,25 +95,24 @@ public:
             if (mouseInArea(event)) return true;
             break;
         case KeyPress:
-//            std::cout << event.xkey.keycode << std::endl;
-            switch(event.xkey.keycode) {
-            case  9:    // ESC
+            char buffer[40];
+            KeySym keysym;
+            XComposeStatus compose;
+            XLookupString(&event.xkey,buffer,10,&keysym,&compose);
+            switch(keysym) {
+            case XK_Escape:
                 return false;
-            case 36:    // ENTER
+            case XK_Return:
                 return true;
-            case 23:    // TAB
+            case XK_Tab:
                 return true;
-            case 22:    // BACKSPACE
+            case XK_BackSpace:
                 if (strlen(text)>1) {
                     text[strlen(text)-2]='_';
                     text[strlen(text)-1]=0;
                 }
                 break;
             default:
-                char buffer[40];
-                KeySym keysym;
-                XComposeStatus compose;
-                XLookupString(&event.xkey,buffer,10,&keysym,&compose);
                 if (keysym==XK_Shift_L || keysym==XK_Shift_R) break;
                 if (keysym>=XK_F1 && keysym<=XK_F35) break;
                 text[strlen(text)-1]=keysym;
@@ -125,6 +139,10 @@ public:
     }
 };
 
+/**
+ * @brief The Button class
+ * ==================================================================================
+ */
 class Button : public Widget {
 public:
     Button(int x,int y,int width,int height,const char text[]) : Widget(x,y,width,height,text){
@@ -133,44 +151,45 @@ public:
     void draw() {
         XDrawRectangle(display,window,gc,x,y,width,height);
         XClearArea(display,window,x+1,y+1,width-2,height-2,false);
-        XDrawString(display,window,gc,x+10,y+height/2+5,text,strlen(text));
+        int cx = x+width/2;
+        int cy = y+height/2;
+        int twidth = strlen(text)*7;
+        int theight = 10;
+        XDrawString(display,window,gc,cx-twidth/2,cy+theight/2,text,strlen(text));
     }
     //-------------------------------------------------------------------------------
     bool triggerEvent(XEvent& event) {
         switch (event.type) {
         case ButtonPress:
-            if (mouseInArea(event)) return true;
+            if (mouseInArea(event)) {
+                (*action)();
+                return true;
+            }
             break;
         }
         return false;
     }
-    //-------------------------------------------------------------------------------
-    void setFocused(bool focused) {
-        if (focused && !this->focused) {
-            strcat(text,"_");
-            draw();
-        }
-        else if (!focused && this->focused) {
-            text[strlen(text)-1]=0;
-            draw();
-        }
-        this->focused = focused;
-    }
 };
 
-
+/**
+ * @brief The Frame class
+ * ==================================================================================
+ */
 class Frame : public Widget {
     static const int   MAX = 100;
 private:
+    bool        active;
+
     Display*    display;
     int         screen;
     Window      window;
     GC          gc;
     Widget*     widgets[MAX];
     Widget*     current;
+
 public:
     Frame(int x,int y,int width,int height,const char title[])
-        : Widget(x,y,width,height,title) {
+        : Widget(x,y,width,height,title), active(true) {
         strcpy(this->text,title);
         display = XOpenDisplay(NULL);
         screen  = DefaultScreen(display);
@@ -193,8 +212,7 @@ public:
     }
     //-------------------------------------------------------------------------------
     void run() {
-        bool keepruning=true;
-        while(keepruning) {
+        while(active) {
             XEvent event;
             XNextEvent(display,&event);
             if (current) if (current->triggerEvent(event)) goto done;
@@ -212,7 +230,7 @@ public:
 
             switch (event.type) {
             case KeyPress:
-                if (event.xkey.keycode==9) keepruning=false;
+                if (event.xkey.keycode==9) active=false;
                 break;
             case Expose:
                 for (int i=0; i<MAX; i++) if (widgets[i]) {
@@ -225,12 +243,17 @@ public:
         }
     }
     //-------------------------------------------------------------------------------
-    void add(Widget* widget) {
+    Widget* add(Widget* widget) {
         for (int i=0; i<MAX; i++) if (!widgets[i]) {
             widgets[i] = widget;
             widgets[i]->updateGraphVariables(display,screen,window,gc);
-            return;
+            return widget;
         }
+        return NULL;
+    }
+    //-------------------------------------------------------------------------------
+    void dispose() {
+        active = false;
     }
 };
 
--- test.cpp
+++ test.cpp
@@ -1,11 +1,17 @@
 #include <simplegui.h>
 
-int main() {
-    Frame frame(10,10,600,400,"My Window");
-    frame.add(new Label(20,20,100,20,"Nombre"));
-    frame.add(new TextField(150,20,300,20));
-    frame.add(new Label(20,50,100,20,"Direccion"));
-    frame.add(new TextField(150,50,300,20));
+Frame* frame;
 
-    frame.run();
+void quit() {
+    frame->dispose();
+}
+
+int main() {
+    frame = new Frame(10,10,600,400,"My Window");
+    frame->add(new Label(20,20,100,20,"Nombre"));
+    frame->add(new TextField(150,20,300,20));
+    frame->add(new Label(20,50,100,20,"Direccion"));
+    frame->add(new TextField(150,50,300,20));
+    frame->add(new Button(20,360,100,20,"Salir"))->action = &quit;
+    frame->run();
 }
